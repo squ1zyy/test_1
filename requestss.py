7822f920ee65a8a3682443518c02fd55192fd484
Kolihay <tpro83840@gmail.com>
2022-09-16 15:11:19 +0300

    'les_3'

--- requestss.py
+++ requestss.py
@@ -1,22 +1,19 @@
 import requests
+from json import dumps
 
 req = requests.get('https://api.openweathermap.org/data/2.5/weather',
                    params={'appid': 'dc4782cd45700c18c29efd99522de225',
                            'units': 'metric',
                            'q': 'Dnipro',
-                           'weather.id': 'Dnipro',
-                           'feels_like.value': 'Dnipro',
-                           'wind.speed.value': 'Dnipro',
-                           'wind.direction.code': 'Dnipro',
-                           'clouds.value': 'Dnipro',
-                           'clouds.name': 'Dnipro',
-                           'visibility.value': 'Dnipro',
-                           })
+                   })
 
 if req.status_code in [200, 202]:
     print(req.status_code, 'code')
-    req.json = req.json()
-
+    req_json = req.json()
+    print(dumps(req_json, indent=4))
+    print(req_json.keys())
+    temperature = req_json["main"]["temp"]
+    print(temperature)
 
 else:
     print('Bad requests.')
